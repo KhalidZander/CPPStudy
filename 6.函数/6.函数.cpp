7761f3a1a4c660c9ac0c807b7ddd0172169672d0
ZanderKhalid <1320565619@qq.com>
2023-05-14 18:16:17 +0800

    123123

--- 6.函数/6.函数.cpp
+++ 6.函数/6.函数.cpp
@@ -1,6 +1,33 @@
 ﻿#include <iostream>
+#include"swap.h";
+using namespace std;
+// 写在main方法之前
+int Add(int num1, int num2)
+{
+	return num1 + num2;
+}
+void Swap(int a, int b)
+{
+	cout << "a:" << a << " b:" << b << endl;
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+	cout << "a:" << a << " b:" << b << endl;
+}
 
+////函数声明
+//int Max(int a, int b);
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << Add(1, 2) << endl;
+	int a = 123,  b = 456;
+	cout << "传值前" << "a:" << a << " b:" << b << endl;
+	Swap(a, b);
+	cout << "传值后" << "a:" << a << " b:" << b << endl;
+
+	cout << "最大值" << Max(10, 20) << endl;
 }
+//int Max(int a, int b)
+//{
+//	return a > b ? a : b;
+//}
